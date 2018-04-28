7aba4940bdb171fb2058a5f1f2a2eff9b3eb27e3
DeeDeeG <DeeDeeG@users.noreply.github.com>
2018-04-27 23:07:48 -0400

    nougat: Create folder for Nougat builds
        Separate folders allows me to keep all editions of each emoji
        in the same Git branch ...
    
        which makes it easier to fix old glyphs, as graphical issues
        are noticed. This also makes it way easier to manage the hybrid fonts
        without so much branch management; i.e. with less cherry-picking
        or duplicate commits across different branches.
    
        (All bug fixes should be able to land at the end of the single
        active development branch.)
    
        The plan is to use only symbolic links to collect the SVGs
        in hybrid folders, and keep originals in the folder for their
        respective Android releases.

---
+++ Nougat/linux/install.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+#https://github.com/DeeDeeG/noto-color-emoji-font
+echo "Noto Color Emoji font installer for Linux\n"
+
+# Check for Bitstream Vera
+fc-list | grep "Bitstream Vera" > /dev/null
+RETURN=$?
+if [ $RETURN -ne 0 ];then
+  echo "Bitstream Vera font family not found. Please install it:"
+  echo "sudo apt-get install ttf-bitstream-vera"
+  exit 1
+fi
+echo "NOTE: Changing default font family to Bitstream Vera"
+
+# Stop on errors
+set -e
+# Set XDG_DATA_HOME to default if empty.
+if [ -z "$XDG_DATA_HOME" ];then
+  XDG_DATA_HOME=$HOME/.local/share
+fi
+
+# Remove font from old directory if exists (temporary backwards compat)
+if [ -f $HOME/.fonts/NotoColorEmoji-SVGinOT.ttf ];then
+  echo "Removing the font from $HOME/.fonts"
+  rm $HOME/.fonts/NotoColorEmoji-SVGinOT.ttf
+fi
+
+# Create a user font directory
+mkdir -p $XDG_DATA_HOME/fonts
+echo "Installing the font in: $XDG_DATA_HOME/fonts/"
+cp NotoColorEmoji-SVGinOT.ttf $XDG_DATA_HOME/fonts/
+
+# Create a font config directory
+FONTCONFIG=$HOME/.config/fontconfig
+mkdir -p $FONTCONFIG/conf.d/
+# Check for an existing font config
+if [ -f $FONTCONFIG/fonts.conf ];then
+  # (temporary backwards compat)
+  echo "Existing fonts.conf backed up to fonts.bak"
+  cp $FONTCONFIG/fonts.conf $FONTCONFIG/fonts.bak
+fi
+# Install fonts.conf
+cp fontconfig/56-noto-color-emoji.conf $FONTCONFIG/conf.d/
+
+echo "Clearing font cache"
+fc-cache -f
+
+echo "Done!"
---
+++ Nougat/windows/install.cmd
@@ -0,0 +1,127 @@
+@ECHO OFF
+SETLOCAL
+
+@setlocal enableextensions
+@cd /d "%~dp0"
+
+SET MS_EMOJI_FONT_PATH="%SystemRoot%\Fonts\seguiemj.ttf"
+SET MS_FONT_PATH="%SystemRoot%\Fonts\seguisym.ttf"
+SET EMOJI_FONT_PATH="%CD%\NotoColorEmoji-SVGinOT.ttf"
+SET FINAL_EMJ_FONT_PATH_NO_QUOTES=%CD%\Segoe UI Emoji with Noto.ttf
+SET FINAL_EMJ_FONT_PATH="%FINAL_EMJ_FONT_PATH_NO_QUOTES%"
+SET FINAL_FONT_PATH_NO_QUOTES=%CD%\Segoe UI Symbol with Noto.ttf
+SET FINAL_FONT_PATH="%FINAL_FONT_PATH_NO_QUOTES%"
+
+ECHO Checking if Segoe UI Emoji is installed
+
+REM Windows 8 uses Segoe UI Emoji in addition to Symbol
+REM Windows 7 only uses Segoe UI Symbol
+REM We have to replace _both_ 
+ECHO Checking if Segoe UI Symbol is installed.
+
+IF NOT EXIST %MS_FONT_PATH% (
+    ECHO.
+    ECHO You don't seem to have the Segoe UI Symbol Font installed.
+    ECHO https://support.microsoft.com/en-us/kb/2729094
+    GOTO :ERROR
+)
+
+ECHO Checking if prerequisites are installed.
+
+WHERE python /q || (
+    ECHO.
+    ECHO Python.exe not found, install or add to PATH.
+    ECHO.
+    GOTO :ERROR
+)
+
+WHERE pip.exe /q || (
+    ECHO.
+    ECHO Pip.exe not found, install or add to PATH
+    ECHO.
+    GOTO :ERROR
+)
+
+ECHO Ensuring the latest FontTools is installed.
+
+pip.exe install --upgrade fonttools
+
+WHERE ttx /q || (
+    ECHO.
+    ECHO ttx.exe not found, please add Python's Scripts folder to PATH
+    ECHO.
+    GOTO :ERROR
+)
+
+PUSHD %TEMP%
+IF EXIST %MS_EMOJI_FONT_PATH% (
+    ECHO Creating new Segoe UI Emoji font from Noto Color Emoji
+    ttx -t "name" -o "emjname.ttx" %MS_EMOJI_FONT_PATH% || GOTO :ERROR
+    ttx -o %FINAL_EMJ_FONT_PATH% -m %EMOJI_FONT_PATH% "emjname.ttx" || GOTO :ERROR
+    DEL "emjname.ttx"
+)
+
+ECHO Creating new Segoe UI Symbol font from Noto Color Emoji
+REM Merge Segoe UI Symbol into Noto Color Emoji -
+REM this keeps Noto Color Emoji's glyph ids intact
+REM for the 'SVG ' table data
+pyftmerge %EMOJI_FONT_PATH% %MS_FONT_PATH%
+ECHO Dumping SVG emojis
+ttx -t "SVG " -o "svg.ttx" %EMOJI_FONT_PATH% || GOTO :ERROR
+ttx -t "name" -o "name.ttx" %MS_FONT_PATH% || GOTO :ERROR
+ECHO Merging in dumped emojis
+ttx -o "almost.ttf" -m "merged.ttf" "name.ttx" || GOTO :ERROR
+DEL "merged.ttf"
+DEL "name.ttx"
+ttx -o %FINAL_FONT_PATH% -m "almost.ttf" "svg.ttx" || GOTO :ERROR
+DEL "almost.ttf"
+DEL "svg.ttx"
+REM Get back to working directory.
+POPD
+
+ECHO.
+ECHO.
+IF EXIST %MS_EMOJI_FONT_PATH% (
+    ECHO The fonts are now saved in
+    ECHO %FINAL_FONT_PATH%
+    ECHO and
+    ECHO %FINAL_EMJ_FONT_PATH%
+    ECHO After installation, the original fonts will still be located at
+    ECHO %MS_FONT_PATH%
+    ECHO and
+    ECHO %MS_EMOJI_FONT_PATH%
+    ECHO They are not overwritten, and can be reinstalled with uninstall.cmd
+) ELSE (
+    ECHO The font is now saved in
+    ECHO %FINAL_FONT_PATH%
+    ECHO After installation, the original font will still be located at
+    ECHO %MS_FONT_PATH%
+    ECHO It is not overwritten, and can be reinstalled with uninstall.cmd
+)
+ECHO To finish installation, the font will be opened for you to install.
+ECHO.
+ECHO If the font is in a network path, copy to a local disk and
+ECHO double click to install.
+ECHO Press the [INSTALL] button in the Font Viewer, then close the viewer.
+CHOICE /m "Would you like to install the fonts now?"
+IF ERRORLEVEL 2 (
+    EXIT /b
+)
+ECHO.
+ECHO Running the font installer for Segoe UI Symbol
+REM The font viewer doesn't like quotes for some reason, but is fine with paths with spaces.
+fontview %FINAL_FONT_PATH_NO_QUOTES%
+if EXIST %MS_EMOJI_FONT_PATH% (
+    ECHO.
+    ECHO Running the font installer for Segoe UI Emoji
+    fontview %FINAL_EMJ_FONT_PATH_NO_QUOTES%
+)
+ECHO.
+ECHO All Done!
+PAUSE
+EXIT /b
+
+:ERROR
+ECHO Installation failed!
+PAUSE
+EXIT /b %ERRORLEVEL%
